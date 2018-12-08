628290caa2fefbaf530f9562565d72075f89d795
YUAN <1763376089@qq.com>
2018-12-08 14:33:53 +0800

    项目基本架构

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <lt-header></lt-header>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+export default {
+  components: {
+    'lt-header': Header
+  },
+  name: 'App',
+  data () {
+    return {
+    }
+  },
+  created () {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="header">
+    <mt-header title="乐淘商城购物">
+    <div slot="left">
+      <mt-button
+      ></mt-button>
+    </div>
+    <mt-button
+      slot="right"
+    >搜索</mt-button>
+  </mt-header>
+</div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="scss" scoped>
+.header{
+    margin: 0px;
+    padding: 0px;
+}
+.mt-header{
+    width: 100%;
+}
+</style>
